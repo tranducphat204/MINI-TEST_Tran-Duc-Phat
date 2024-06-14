9a1d84cd7fd415527d30be6f65c4f534824b2e07
phatdeptraiii <vitphat350@gmail.com>
2024-06-14 16:37:52 +0700

    done ex2

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -1,7 +1,8 @@
 import Header from "./components/Header";
 import Scores from "./components/Scores";
 import { JAVA_RESULTS } from "./data";
-
+import { PYTHON_RESULTS } from "./data";
+import { ENGLISH_RESULTS } from "./data";
 function App() {
   return (
     <>
@@ -9,6 +10,8 @@ function App() {
 
       <main className="scores-container">
         <Scores courseName="HTML" courseResult={JAVA_RESULTS} />
+        <Scores courseName="PYTHON" courseResult={PYTHON_RESULTS} />
+        <Scores courseName="ENGLISH" courseResult={ENGLISH_RESULTS} />
       </main>
     </>
   );
